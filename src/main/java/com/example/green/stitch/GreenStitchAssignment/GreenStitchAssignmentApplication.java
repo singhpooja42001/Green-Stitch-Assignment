1e485937671777742e8c3baa3fe52f08d76ec037
Pooja Singh <poojasingh42001@gmail.com>
2023-06-11 21:47:13 +0530

    first commit

---
+++ src/main/java/com/example/green/stitch/GreenStitchAssignment/GreenStitchAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.green.stitch.GreenStitchAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenStitchAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenStitchAssignmentApplication.class, args);
+	}
+
+}
